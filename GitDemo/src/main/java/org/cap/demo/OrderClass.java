f5bcfd2c5855935108408c6d4b602f4a34e28cd9
lnc <lnc@DIN76000837.corp.capgemini.com>
2017-11-29 11:56:36 +0530

    sum added

--- GitDemo/src/main/java/org/cap/demo/OrderClass.java
+++ GitDemo/src/main/java/org/cap/demo/OrderClass.java
@@ -5,10 +5,13 @@ public class OrderClass {
 	int a[]={1,2,3,4,5,6};
 	public void display()
 	{
+		int sum=0;
 		for(int i=0;i<5;i++)
 		{
+			sum+=a[i];
 			System.out.println(a[i]);
 		}
+		System.out.println("sum is "+sum);
 	}
 	public void show()
 	{
